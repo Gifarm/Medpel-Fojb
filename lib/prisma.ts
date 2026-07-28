5f48c01a1950da44e744b6af5a2c725c90f96eaf
Muhammad Gifar <gantenggifar4@gmail.com>
2026-07-28 22:05:40 +0700

    perbaikan1

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,14 @@
+// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
+// Gunakan Environment Variables agar fleksibel untuk local dan Vercel
 const adapter = new PrismaMariaDb({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "medpel_db",
+  host: process.env.DB_HOST || "127.0.0.1",
+  port: parseInt(process.env.DB_PORT || "3306", 10),
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "medpel_db",
   connectionLimit: 5,
 });
 
